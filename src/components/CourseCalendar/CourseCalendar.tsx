5c9ad104ea863fafc8bd5f65bdebbf9ac72587b1
Adrian González <adrigondo.profesional@gmail.com>
2023-09-09 20:57:05 -0600

    Update: adding floating tootlip to events

--- src/components/CourseCalendar/CourseCalendar.tsx
+++ src/components/CourseCalendar/CourseCalendar.tsx
@@ -5,35 +5,26 @@ import esLocale from '@fullcalendar/core/locales/es';
 import { DateRangeInput, EventInput } from "fullcalendar/index.js";
 import { useEffect, useState } from "react";
 import './CourseCalendar.css';
-
-const EventContent = (eventInfo: EventInput) => {
-  let start = new Date(eventInfo.event.start).getHours();
-  let end = new Date(eventInfo.event.end).getHours();
-  const startIsPm = Math.floor(start / 12) ? true : false;
-  const endIsPm = Math.floor(end / 12) ? true : false;
-  start = start % 12;
-  end = end % 12
-  return (
-    <div className="custom-event">
-      <b>{start}{startIsPm ? 'pm' : 'am'} - {end}{endIsPm ? 'pm' : 'am'}</b><br></br>
-      <label className="fc-event-title">{eventInfo.event.title}</label>
-    </div>
-  );
-}
+import EventContent from "./EventContent/EventContent";
 
 const CourseCalendar = (
   { meetings, visibleRange, numberOfMonths }:
     { meetings: any, visibleRange?: DateRangeInput, numberOfMonths?: number }
 ) => {
+
   visibleRange = visibleRange ? visibleRange : {
     start: new Date(2023, 7, 14),
     end: new Date(2023, 11, 1),
   };
+
   const events: EventInput[] = meetings.map((meeting: any): EventInput => {
     return {
       title: meeting.title,
       start: meeting.start,
       end: meeting.end,
+      extendedProps: {
+        content: meeting.content,
+      }
     }
   });
 
@@ -47,34 +38,42 @@ const CourseCalendar = (
       .addEventListener('change', e => setMatches(e.matches));
   }, []);
 
-  return <div className="w-100">
-    <FullCalendar
-      plugins={[dayGridPlugin, multiMonthPlugin]}
-      initialView={"customMultiMonth"}
-      defaultAllDay={false}
-      locale={esLocale}
-      events={events}
-      initialDate={visibleRange.start}
-      views={{
-        customMultiMonth: {
-          type: 'multiMonthYear',
-          duration: { months: numberOfMonths || 4 },
-          visibleRange: visibleRange,
-          multiMonthMaxColumns: 2,
-          initialDate: visibleRange.start,
-          weekends: false,
-        }
-      }}
-      eventContent={EventContent}
-      headerToolbar={{
-        center: '',
-        left: '',
-        right: '',
-      }}
-      contentHeight={'auto'}
+  // const showEventContent = (arg: EventClickArg) => {
+  //   console.log(arg.event.extendedProps.reference);
+  //   refs.setReference(arg.event.extendedProps.reference.current);
+  // }
 
-      aspectRatio={matches ? 1.35 : 1}
-    ></FullCalendar>
-  </div >
+  return (
+    <div className="w-100">
+      <FullCalendar
+        plugins={[dayGridPlugin, multiMonthPlugin]}
+        initialView={"customMultiMonth"}
+        defaultAllDay={false}
+        locale={esLocale}
+        events={events}
+        initialDate={visibleRange.start}
+        views={{
+          customMultiMonth: {
+            type: 'multiMonthYear',
+            duration: { months: numberOfMonths || 4 },
+            visibleRange: visibleRange,
+            multiMonthMaxColumns: 2,
+            initialDate: visibleRange.start,
+            weekends: false,
+          }
+        }}
+        eventContent={(data) => <EventContent data={data} />}
+        headerToolbar={{
+          center: '',
+          left: '',
+          right: '',
+        }}
+        // eventClick={(arg) => showEventContent(arg)}
+        contentHeight={'auto'}
+
+        aspectRatio={matches ? 1.35 : 1}
+      ></FullCalendar>
+    </div >
+  )
 }
 export default CourseCalendar;
---
+++ src/components/CourseCalendar/EventContent/EventContent.tsx
@@ -0,0 +1,66 @@
+import { FloatingArrow, arrow, offset, safePolygon, shift, useClick, useDismiss, useFloating, useFocus, useHover, useInteractions, useRole, useTransitionStyles } from "@floating-ui/react";
+import { EventInput } from "fullcalendar/index.js";
+import { useRef, useState } from "react";
+import './EventContent.css';
+
+const EventContent = ({ data }: { data: any }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const arrowRef = useRef<any>(null);
+  const { refs, floatingStyles, context } = useFloating({
+    open: isOpen,
+    onOpenChange: setIsOpen,
+    placement: "top",
+    middleware: [
+      shift(),
+      arrow({
+        element: arrowRef,
+      }),
+      offset(-5),
+    ],
+  });
+  const focus = useFocus(context);
+  const click = useClick(context);
+  const role = useRole(context, { role: 'tooltip' });
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    focus,
+    click,
+    role,
+  ]);
+  const { styles } = useTransitionStyles(context);
+
+  const { event } = data;
+
+  let start = new Date(event.start).getHours();
+  let end = new Date(event.end).getHours();
+  const startIsPm = Math.floor(start / 12) ? true : false;
+  const endIsPm = Math.floor(end / 12) ? true : false;
+  start = start % 12;
+  end = end % 12
+
+  return (
+    <>
+      <div className="custom-event" ref={refs.setReference} {...getReferenceProps()}>
+        <div className="event-main">
+          <b>{start}{startIsPm ? 'pm' : 'am'} - {end}{endIsPm ? 'pm' : 'am'}</b><br></br>
+          <label className="fc-event-title">{event.title}</label>
+        </div>
+      </div>
+      {
+        isOpen &&
+        <div
+          ref={refs.setFloating}
+          style={{ ...floatingStyles, ...styles }}
+
+          {...getFloatingProps()}
+          className="event-content"
+        >
+          {event.extendedProps.content}
+
+          <FloatingArrow ref={arrowRef} context={context} className="event-arrow" rotate='45deg' />
+        </div>
+      }
+    </>
+  );
+}
+
+export default EventContent;
